38281ddf2633a7a4fe07bc3171db9d1e28a9e937
Tsiolis Konstantinos <tsiolis.kostas@gmail.com>
2014-06-08 19:04:55 +0300

    find recursive into multidimension array

--- server/system/functions.php
+++ server/system/functions.php
@@ -58,4 +58,18 @@ function humanTiming($time)
     return $value;
 }
 
+//work recursive in php multidimensions array from php.net comment
+function array_key_exists_r($needle, $haystack)
+{
+    $result = array_key_exists($needle, $haystack);
+    if ($result) return $result;
+    foreach ($haystack as $v) {
+        if (is_array($v)) {
+            $result = array_key_exists_r($needle, $v);
+        }
+        if ($result) return $result;
+    }
+    return $result;
+}
+
 ?>
