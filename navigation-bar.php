8ad032d097de3b0786f1f5770322cd343c7c1e28
Chrysanthi <ubichrys@gmail.com>
2014-06-18 12:36:37 +0300

    new stuff for navigation bar

---
+++ navigation-bar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--navigation bar-->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <img style="float:left; padding-left:10px; padding-top:7px" alt="ΠΣΔ" src="client/pages/icons/sch-icon38px.png">
+        <!--<img src="client/img/logo-teia.jpg" alt="ΤΕΙ Αθήνας" style="float:left; padding-left:15px;">-->
+        <a class="navbar-brand" href="index.php" style="font-size:30px;">Υπηρεσία MyLab </a>
+        <!--<span class="label label-danger" style="position:absolute; left:7px;top:46px;">BETA Version</span>-->
+    </div>
+</nav>
