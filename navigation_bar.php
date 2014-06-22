9760a87d501ca23eaa4dfa2404a2ff497d424346
Chrysanthi <ubichrys@gmail.com>
2014-06-22 21:08:22 +0300

    added reports dd button

--- navigation_bar.php
+++ navigation_bar.php
@@ -11,7 +11,28 @@
         </button>
         <img style="float:left; padding-left:10px; padding-top:7px" alt="ΠΣΔ" src="client/pages/icons/sch-icon38px.png">
         <!--<img src="client/img/logo-teia.jpg" alt="ΤΕΙ Αθήνας" style="float:left; padding-left:15px;">-->
-        <a class="navbar-brand" href="index.php" style="font-size:30px;">Υπηρεσία MyLab </a>
+        <a class="navbar-brand" href="index.php" style="font-size:30px;">Υπηρεσία MyLab </a> 
         <!--<span class="label label-danger" style="position:absolute; left:7px;top:46px;">BETA Version</span>-->
     </div>
+        
+    <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Αναφορές <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="http://mmsch.teiath.gr/mylab/api/labs?edu_admin=4">Αναφορά 1</a></li>
+                </ul>
+            </li>
+          <li><div style="margin:10px 30px"><a href="#" id="lnkLogout" class="btn btn-sm btn-warning"></a></div></li>
+        </ul>
+    </div>    
 </nav>
+
+
+<script>
+    var user = JSON.parse(atob("<?php echo base64_encode(json_encode($user)); ?>")); 
+    var g_casUrl = "<?php echo $casOptions['Url'] ?>";
+    // Build logout link
+    $("#lnkLogout").attr("href", "http://" + g_casUrl + "/logout");
+    $("#lnkLogout").html("<strong>" + user.uid + " [Logout]" + "</strong>");
+</script>
