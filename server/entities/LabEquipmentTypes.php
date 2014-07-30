46395f41d4d725429cfd9c9d036f7b669dd2c33a
Tsiolis Konstantinos <tsiolis.kostas@gmail.com>
2014-07-30 10:57:34 +0300

    fixed code

--- server/entities/LabEquipmentTypes.php
+++ server/entities/LabEquipmentTypes.php
@@ -14,11 +14,14 @@ class LabEquipmentTypes
 {
     
     /**
-     * @ORM\@Id @ORM\@ManyToOne(targetEntity="Labs", inversedBy="Labs") 
+     * @var \Labs
      *
-
+     * @ORM\Column(name="lab_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="Labs")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="lab_id", referencedColumnName="lab_id")
      * })
-     * 
      */
     private $labId;
     
