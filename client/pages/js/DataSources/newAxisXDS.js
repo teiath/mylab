febc4ea625744e33bd37bcba1268697bc8d8d1da
Chrysanthi <ubichrys@gmail.com>
2014-09-16 13:15:08 +0300

    splitted newAxisDS to two, one for each axis

---
+++ client/pages/js/DataSources/newAxisXDS.js
@@ -0,0 +1,31 @@
+function newAxisXDS(){
+    
+    var axis_x_ds =  new kendo.data.DataSource({
+                                    data: [
+                                            { axis_id: 1, axis_name: "lab_type", name: "τύπος Διάταξης Η/Υ"},
+                                            { axis_id: 2, axis_name: "lab_state", name: "λειτουργική κατάσταση Διάταξης Η/Υ"},
+                                            { axis_id: 3, axis_name: "school_unit_type", name: "τύπος Σχολικής Μονάδας"}, 
+                                            { axis_id: 4, axis_name: "school_unit_state", name: "λειτουργική κατάσταση Σχολικής Μονάδας"},
+                                            { axis_id: 5, axis_name: "education_level", name: "Βαθμίδα Εκπαίδευσης"},
+                                            { axis_id: 6, axis_name: "region_edu_admin", name: "Περιφερειακή Διεύθυνση Εκπαίδευσης"},
+                                            { axis_id: 7, axis_name: "edu_admin", name: "Διεύθυνση Εκπαίδευσης"},
+                                            { axis_id: 8, axis_name: "transfer_area", name: "Περιοχή Μετάθεσης"},
+                                            { axis_id: 9, axis_name: "municipality", name: "Δήμος"},
+                                            { axis_id: 10, axis_name: "prefecture", name: "Νομός"}],
+                                    schema: {
+                                        model:{
+                                            id: "axis_name",
+                                            fields:{
+                                                axis_id: {editable: false, type: "number"},
+                                                axis_name:{editable: false, type: "string"},
+                                                name:{editable: false, type: "string"}
+                                            }
+                                        }
+                                    }
+                                    //filter: { field: "used", operator: "neq", value: true },
+                                    //change: function(e){ console.log("newRatingDS change event:", e);},
+                                    //requestEnd: function(e){console.log("newRatingDS requestEnd event:", e);}
+                                });
+    return axis_x_ds;
+    
+}
---
+++ client/pages/js/DataSources/newAxisYDS.js
@@ -0,0 +1,32 @@
+function newAxisYDS(){
+    
+    var axis_y_ds =  new kendo.data.DataSource({
+                                    data: [ 
+                                            { axis_id: 1, axis_name: "lab_type", name: "τύπος Διάταξης Η/Υ", parent:"lab_type"},
+                                            { axis_id: 2, axis_name: "lab_state", name: "λειτουργική κατάσταση Διάταξης Η/Υ", parent:"lab_state"},
+                                            { axis_id: 3, axis_name: "school_unit_type", name: "τύπος Σχολικής Μονάδας", parent:"school_unit_type"}, 
+                                            { axis_id: 4, axis_name: "school_unit_state", name: "λειτουργική κατάσταση Σχολικής Μονάδας", parent:"school_unit_state"},
+                                            { axis_id: 5, axis_name: "education_level", name: "Βαθμίδα Εκπαίδευσης" , parent:"education_level"},
+                                            { axis_id: 6, axis_name: "region_edu_admin", name: "Περιφερειακή Διεύθυνση Εκπαίδευσης" , parent:"region_edu_admin"},
+                                            { axis_id: 7, axis_name: "edu_admin", name: "Διεύθυνση Εκπαίδευσης" , parent:"edu_admin"},
+                                            { axis_id: 8, axis_name: "transfer_area", name: "Περιοχή Μετάθεσης" , parent:"transfer_area"},
+                                            { axis_id: 9, axis_name: "municipality", name: "Δήμος" , parent:"municipality"},
+                                            { axis_id: 10, axis_name: "prefecture", name: "Νομός" , parent:"prefecture"}],
+                                    schema: {
+                                        model:{
+                                            id: "axis_name",
+                                            fields:{
+                                                axis_id: {editable: false, type: "number"},
+                                                axis_name:{editable: false, type: "string"},
+                                                name:{editable: false, type: "string"},
+                                                parent:{editable: false, type: "string"}
+                                            }
+                                        }
+                                    }
+                                    //filter: { field: "used", operator: "neq", value: true },
+                                    //change: function(e){ console.log("newRatingDS change event:", e);},
+                                    //requestEnd: function(e){console.log("newRatingDS requestEnd event:", e);}
+                                });
+    return axis_y_ds;
+    
+}
