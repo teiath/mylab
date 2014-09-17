c930b4a05020623c421a244ac2c71c776eebd02d
Chrysanthi <ubichrys@gmail.com>
2014-09-17 09:59:22 +0300

    fix chrome bug in navigation bar

--- navigation_bar.php
+++ navigation_bar.php
@@ -2,7 +2,7 @@
 <!--navigation bar-->
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="background-color:#5E5E5E;">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
+    <div class="navbar-header" style="width: 500px;">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
