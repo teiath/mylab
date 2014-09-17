6b824b91b815d7344fafd7c8fc77e7b3108fc2dd
Chrysanthi <ubichrys@gmail.com>
2014-09-17 15:31:00 +0300

    minor css changes

--- info.php
+++ info.php
@@ -3,26 +3,47 @@
 <div id="info-container" class="container" style="font-size: 13px; color: #787878" data-bind="visible: isVisible">
     <div class="row">
         <div class="col-md-12" style="font-size: 13px; color: #787878;">
-                <div class="col-md-11" >
-                    <label> χρήστης: </label>
+                <div class="col-md-8 col-md-offset-3" >
+                    <div class="col-md-3" style="text-align: right;">
+                        <b> χρήστης: </b>
+                    </div>
+                    <div class="col-md-8" ></div>
                 </div>
-                <div class="col-md-11" >
-                    <label> ρόλος: </label>
+                <div class="col-md-8 col-md-offset-3" >
+                    <div class="col-md-3" style="text-align: right;">
+                        <b> ρόλος: </b>
+                    </div>
+                    <div class="col-md-8" ></div>
                 </div>
-                <div class="col-md-11" >
-                    <label> μονάδα: </label>
+                <div class="col-md-8 col-md-offset-3" >
+                    <div class="col-md-3" style="text-align: right;">
+                        <b> μονάδα: </b>
+                    </div>
+                    <div class="col-md-8" ></div>
                 </div>
-                <div class="col-md-11" >
-                    <label> τηλ: </label>
+                <div class="col-md-8 col-md-offset-3" >
+                    <div class="col-md-3" style="text-align: right;">
+                        <b> τηλ: </b>
+                    </div>
+                    <div class="col-md-8" ></div>
                 </div>
-                <div class="col-md-11" >
-                    <label> φαξ: </label>
+                <div class="col-md-8 col-md-offset-3" >
+                    <div class="col-md-3" style="text-align: right;">
+                        <b> φαξ: </b>
+                    </div>
+                    <div class="col-md-8" ></div>
                 </div>
-                <div class="col-md-11" >
-                    <label> e-mail: </label>
+                <div class="col-md-8 col-md-offset-3" >
+                    <div class="col-md-3" style="text-align: right;">
+                        <b> e-mail: </b>
+                    </div>
+                    <div class="col-md-8" ></div>
                 </div>
-                <div class="col-md-11" >
-                    <label> ταχυδρομική διεύθυνση: </label>
+                <div class="col-md-8 col-md-offset-3" >
+                    <div class="col-md-3" style="text-align: right;">
+                        <b> ταχυδρομική διεύθυνση: </b>
+                    </div>
+                    <div class="col-md-8" ></div>
                 </div>
         </div>
     </div>
