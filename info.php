bde547771359b34d68d2b9e074569048aa180109
Chrysanthi <ubichrys@gmail.com>
2014-09-18 15:48:58 +0300

    fix responsivness bug in info view

--- info.php
+++ info.php
@@ -2,48 +2,34 @@
 
 <div id="info-container" class="container" style="font-size: 13px; color: #787878" data-bind="visible: isVisible">
     <div class="row">
-        <div class="col-md-12" style="font-size: 13px; color: #787878;">
-                <div class="col-md-8 col-md-offset-3" >
-                    <div class="col-md-3" style="text-align: right;">
-                        <b> χρήστης: </b>
-                    </div>
-                    <div class="col-md-8" ></div>
+        <div class="col-md-12" style="font-size: 13px; color: #787878; text-align: center;">
+                <div class="col-md-12" >
+                    <span><b> χρήστης: </b></span>
+                    <span style="text-align: right; margin-left:5px;">etyrtyrthyrtjhrtjrtj</span>
                 </div>
-                <div class="col-md-8 col-md-offset-3" >
-                    <div class="col-md-3" style="text-align: right;">
-                        <b> ρόλος: </b>
-                    </div>
-                    <div class="col-md-8" ></div>
+                <div class="col-md-12" >
+                    <span><b> ρόλος: </b></span>
+                    <span style="text-align: right; margin-left:5px;">etyrtyrthyrtjhrtjrtj</span>
                 </div>
-                <div class="col-md-8 col-md-offset-3" >
-                    <div class="col-md-3" style="text-align: right;">
-                        <b> μονάδα: </b>
-                    </div>
-                    <div class="col-md-8" ></div>
+                <div class="col-md-12" >
+                    <span><b> μονάδα: </b></span>
+                    <span style="text-align: right; margin-left:5px;">etyrtyrthyrtjhrtjrtj</span>
                 </div>
-                <div class="col-md-8 col-md-offset-3" >
-                    <div class="col-md-3" style="text-align: right;">
-                        <b> τηλ: </b>
-                    </div>
-                    <div class="col-md-8" ></div>
+                <div class="col-md-12" >
+                    <span><b> τηλέφωνο: </b></span>
+                    <span style="text-align: right; margin-left:5px;">etyrtyrthyrtjhrtjrtj</span>
                 </div>
-                <div class="col-md-8 col-md-offset-3" >
-                    <div class="col-md-3" style="text-align: right;">
-                        <b> φαξ: </b>
-                    </div>
-                    <div class="col-md-8" ></div>
+                <div class="col-md-12" >
+                    <span><b> φαξ: </b></span>
+                    <span style="text-align: right; margin-left:5px;">etyrtyrthyrtjhrtjrtj</span>
                 </div>
-                <div class="col-md-8 col-md-offset-3" >
-                    <div class="col-md-3" style="text-align: right;">
-                        <b> e-mail: </b>
-                    </div>
-                    <div class="col-md-8" ></div>
+                <div class="col-md-12" >
+                    <span><b> email: </b></span>
+                    <span style="text-align: right; margin-left:5px;">etyrtyrthyrtjhrtjrtj</span>
                 </div>
-                <div class="col-md-8 col-md-offset-3" >
-                    <div class="col-md-3" style="text-align: right;">
-                        <b> ταχυδρομική διεύθυνση: </b>
-                    </div>
-                    <div class="col-md-8" ></div>
+                <div class="col-md-12" >
+                    <span><b> διεύθυνση: </b></span>
+                    <span style="text-align: right; margin-left:5px;">etyrtyrthyrtjhrtjrtj</span>
                 </div>
         </div>
     </div>
