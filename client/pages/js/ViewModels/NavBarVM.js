2d0034ce2c12fa7406b043880bd241ef0a402a8e
Chrysanthi <ubichrys@gmail.com>
2014-10-03 12:37:29 +0300

    user guide dieuthyntes v3 updated

--- client/pages/js/ViewModels/NavBarVM.js
+++ client/pages/js/ViewModels/NavBarVM.js
@@ -58,7 +58,7 @@ var NavBarVM = kendo.observable({
         }else if(authorized_user === "ΠΣΔ" ){
             //url = "http://myfiles.sch.gr/server/get/d7701f86e3fcc44238c18f2b6b08cad4b"; //config.url + "client/user_guides/psd.pdf";
         }else if(authorized_user === "ΔΙΕΥΘΥΝΤΗΣ" ){
-            url = "http://myfiles.sch.gr/server/get/db2fb7b4f5f3a45c996e2d4470b01b587"; //config.url + "client/user_guides/dieuthintis.pdf";
+            url = "http://myfiles.sch.gr/server/get/d71f46c9bae10433fab91dc54fd1b1c34"; //config.url + "client/user_guides/dieuthintis.pdf";
         }else if(authorized_user === "ΥΠΕΠΘ" ){
             //url = "http://myfiles.sch.gr/server/get/d7701f86e3fcc44238c18f2b6b08cad4b"; //config.url + "client/user_guides/upepth.pdf";
         }
