a4102363d9dee55092ab690fe2e73ed88aa393ff
Chrysanthi <ubichrys@gmail.com>
2014-12-03 19:05:03 +0200

    view and view-model for new view, lab workers

--- lab_workers_view.php
+++ lab_workers_view.php
@@ -6,12 +6,22 @@
         /* notification templates */
         require_once('client/pages/js/Templates/notification_template/errorNotificationTemplate.html');
         require_once('client/pages/js/Templates/notification_template/successNotificationTemplate.html');
+        /* grid toolbar templates */
+        require_once('client/pages/js/Templates/grid_toolbar_template/labToolbarTemplate_lab_workers_view.html');
+        require_once('client/pages/js/Templates/grid_toolbar_template/labToolbarTemplate_lab_workers_labs.html');
+        /* grid detail templates */
+        require_once('client/pages/js/Templates/grid_detail_template/labWorkerDetailsTemplate.html');
+        /* grid toolbar command popup dialog templates */
+        require_once('client/pages/js/Templates/grid_toolbar_command_popup_dialog_template/labWorkersColumnSelectionTemplate.html');
+        require_once('client/pages/js/Templates/grid_column_template/labWorkersViewFullnameColumnTemplate.html');
+        /* grid column command popup dialog templates */
+        require_once('client/pages/js/Templates/grid_column_command_popup_dialog_template/schoolUnitContactDetailsTemplate.html');
         
     ?>
   
 
-    <!--transition notification-->
-    <span id="notification" style="display:none;"></span>
+    <!--column selection dialog-->
+    <div id="lab_workers_column_selection_dialog"></div>
 
     <!-- grid element -->
     <div id="general-container" class="container">      
@@ -20,27 +30,27 @@
                 <div    id="lab_workers_view"
                         data-role="grid"
                         data-bind="source: lab_workers, visible: isVisible"
-                        data-detail-init="LabsViewVM.detailInit"
+                        data-detail-init="LabWorkersViewVM.detailInit"
+                        data-detail-template= 'lab_worker_details_template'
                         data-selectable="row"
                         data-scrollable= "true"
                         data-resizable= "true"
                         data-sortable= "{'allowUnsort': false}"
                         data-pageable="{ 'pageSizes': [5, 10, 15, 20, 25, 30, 50], 
-                                         'messages':  { 'display': '{0}-{1} από {2} Διατάξεις Η/Υ', 
-                                                        'empty': 'Δεν βρέθηκαν Διατάξεις Η/Υ',
-                                                        'itemsPerPage': 'Διατάξεις Η/Υ ανά σελίδα', 
-                                                        'first': 'μετάβαση στην πρώτη σελίδα',
-                                                        'previous': 'μετάβαση στην προηγούμενη σελίδα',
-                                                        'next': 'μετάβαση στην επόμενη σελίδα',
-                                                        'last': 'μετάβαση στην τελευταία σελίδα' }}"
-                        data-columns="[{ 'field': 'worker_id', 'title':'Κωδικός', 'width':'150px'},
-                                       { 'field': 'firstname', 'title':'Όνομα', 'width':'150px'},
-                                       { 'field': 'lastname', 'title':'Επώνυμο', 'width':'150px'},
-                                       { 'field': 'fathername', 'title':'Όνομα Πατρός', 'width':'150px'},
-                                       { 'field': 'worker_registry_no', 'title':'Αρ. Μητρώου', 'width':'95px'},
-                                       { 'field': 'tax_number', 'title':'ΑΦΜ', 'width':'100px',},
-                                       { 'field': 'sex', 'title':'Φύλο', 'width':'100px', },
-                                       { 'field': 'specialization_code_name', 'title':'Κλάδος', 'width':'100px'}]"
+                                         'messages':  { 'display': '{0}-{1} από {2} Υπεύθυνοι Διατάξεων Η/Υ', 
+                                                        'empty': 'Δεν βρέθηκαν Υπεύθυνοι Διατάξεων Η/Υ',
+                                                        'itemsPerPage': 'Υπεύθυνοι Διατάξεων Η/Υ ανά σελίδα', 
+                                                        'first': 'πρώτη σελίδα',
+                                                        'previous': 'προηγούμενη σελίδα',
+                                                        'next': 'επόμενη σελίδα',
+                                                        'last': 'τελευταία σελίδα' }}"
+                        data-toolbar="[{ 'template' : $('#lab_toolbar_template_lab_workers_view').html()  }]"
+                        data-columns="[{ 'field': 'lastname', 'title': 'Ονοματεπώνυμο', 'template' : $('#lab_workers_view_fullname_column_template').html(), 'width':'200px'},
+                                       { 'field': 'fathername', 'title':'Όνομα Πατρός', 'width':'100px'},
+                                       { 'field': 'registry_no', 'title':'Αρ. Μητρώου', 'width':'100px'},
+                                       { 'field': 'worker_uid', 'title':'Ldap UID', 'width':'120px'},
+                                       { 'field': 'email', 'title':'E-mail', 'width':'170px'},
+                                       { 'field': 'worker_specialization_name', 'title':'Κλάδος', 'width':'70px'}]"
                     >
                 </div>
             </div>
