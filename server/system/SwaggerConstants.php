b6d33089b910633a7979d61d03d5beb44741c9bf
Tsiolis Konstantinos <tsiolis.kostas@gmail.com>
2015-01-13 14:09:39 +0200

    fixed relartive infos

--- server/system/SwaggerConstants.php
+++ server/system/SwaggerConstants.php
@@ -1,4 +1,15 @@
 <?php
+
+    //mmsch server
+    define('SERVER_DOCS_PATH','http://mmsch.teiath.gr/mylab/server/libs/swagger-ui-master/dist/index.html');
     define('BASE_PATH', 'http://mmsch.teiath.gr/mylab/api');
-    //define('BASE_PATH', 'https://mylab.sch.gr/api');
+    define('API_VERSION', '2.0');
+    define('SWAGGER_VERSION', 2.0);
+      
+    //mm server
+    //define('SERVER_DOCS_PATH','http://mylab.sch.gr/server/libs/swagger-ui-master/dist/index.html');
+    //define('BASE_PATH', 'http://mylab.sch.gr/api');
+    //define('API_VERSION', '2.0');
+    //define('SWAGGER_VERSION', '2.0');
+    
 ?>
