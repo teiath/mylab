3a93d7c1ced119b9b646ef5327e422547613d2ae
Tsiolis Konstantinos <tsiolis.kostas@gmail.com>
2015-02-04 12:58:35 +0200

    finished documentation

--- docs/index.php
+++ docs/index.php
@@ -1,4 +1,9 @@
 <?php
+
+chdir("../server");
+require_once('system/includes.php');
+Header("Content-Type: text/html; charset=utf-8");
+Header('Content-Type: application/json');
 Header( "HTTP/1.1 301 Moved Permanently" );
 Header( "Location:".SERVER_DOCS_PATH );
 ?>
--- server/system/SwaggerConstants.php
+++ server/system/SwaggerConstants.php
@@ -1,4 +1,19 @@
 <?php
+//Για την δημιουργία documentation με την βιβλιοθηκη swagger-php χρησιμοποιούμε annotations.
+//Υπαρχει παράδειγμα με annotations στο φάκελο /api.
+//Η σύνταξη των annotations είναι πολυ συγκεκριμένη και υπάρχει εκτενής αναφορά στο
+//http://zircote.com/swagger-php/index.html
+//και επίσης στο
+//https://github.com/swagger-api/swagger-spec/blob/master/versions/1.2.md
+//
+//Αφού γίνει η σύνταξη των annotations σε κάθε function που μας ενδιαφερει να κάνουμε documentation
+//εκτελούμε την παρακάτω εντολή :
+// php server/libs/doctrine/vendor/zircote/swagger-php/swagger.phar --bootstrap server/system/SwaggerConstants.php,server/exceptions/ExceptionCodes.php,server/exceptions/ExceptionMessages.php 
+// api/get api/post api/put api/del api/inedx.php -o server/generate-docs
+// 
+//Η παραπάνω εντολή με βάση τα annotations που έχουμε συντάξει στα αρχεία του καταλόγου /api
+//δημιουργεί json αρχεία στον κατάλογο "/server/generate-docs" κατάλληλα διαμορφωμένα ώστε για να μπορούν να διαβαστούν
+//από το swagger-ui και να έχουμε ένα pretty-style documentation.
 
     //mmsch server
     define('SERVER_DOCS_PATH','http://mmsch.teiath.gr/mylab/server/libs/swagger-ui-master/dist/index.html');
