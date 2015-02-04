41ddb73bc5e6d2e3feb23604329f581a79a7731c
Tsiolis Konstantinos <ktsiolis@teiath.gr>
2015-02-04 17:05:36 +0200

    reconfigure mylab server conf files

--- server/system/SwaggerConstants.php
+++ server/system/SwaggerConstants.php
@@ -16,15 +16,15 @@
 //από το swagger-ui και να έχουμε ένα pretty-style documentation.
 
     //mmsch server
-    define('SERVER_DOCS_PATH','http://mmsch.teiath.gr/mylab/server/libs/swagger-ui-master/dist/index.html');
-    define('BASE_PATH', 'http://mmsch.teiath.gr/mylab/api');
-    define('API_VERSION', '2.0');
-    define('SWAGGER_VERSION', 2.0);
+//    define('SERVER_DOCS_PATH','http://mmsch.teiath.gr/mylab/server/libs/swagger-ui-master/dist/index.html');
+//    define('BASE_PATH', 'http://mmsch.teiath.gr/mylab/api');
+//    define('API_VERSION', '2.0');
+//    define('SWAGGER_VERSION', 2.0);
       
     //mm server
-    //define('SERVER_DOCS_PATH','http://mylab.sch.gr/server/libs/swagger-ui-master/dist/index.html');
-    //define('BASE_PATH', 'http://mylab.sch.gr/api');
-    //define('API_VERSION', '2.0');
-    //define('SWAGGER_VERSION', '2.0');
+     define('SERVER_DOCS_PATH','https://mylab.sch.gr/server/libs/swagger-ui-master/dist/index.html');
+     define('BASE_PATH', 'https://mylab.sch.gr/api');
+     define('API_VERSION', '2.0');
+     define('SWAGGER_VERSION', 2.0);
     
 ?>
